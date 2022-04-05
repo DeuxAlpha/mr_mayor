1542a6057910ad6d7f68f49c76ada0e0cc67b78b
Aaron Parsons <ap007@aquinas.edu>
2022-04-05 11:22:27 -0400

    Moved namespaces

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,4 @@
-﻿namespace DefaultNamespace
+﻿public static class GameManager
 {
-    public static class GameManager
-    {
-        public static float _playerHealth = 100.0001f;
-    }
+    public static float _playerHealth = 100.0001f;
 }
--- Assets/Scripts/SimplePlayerController.cs
+++ Assets/Scripts/SimplePlayerController.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
-using DefaultNamespace;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
 
